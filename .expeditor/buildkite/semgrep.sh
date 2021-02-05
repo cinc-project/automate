56b73fb809ee38cd73a6af05d1b5bb5dd6443846
Michael Sorens <msorens@chef.io>
2021-02-04 18:20:49 -0800

    more scripting
    Signed-off-by: michael sorens <msorens@chef.io>

--- .expeditor/buildkite/semgrep.sh
+++ .expeditor/buildkite/semgrep.sh
@@ -12,9 +12,13 @@ export SEMGREP_JOB_URL=$2
 # Activates links to git commits in slack notification
 export SEMGREP_REPO_URL=https://github.com/chef/automate
 BASELINE=$3
-MERGE_BASE=$(git merge-base ${BASELINE:-master} HEAD)
+MERGE_BASE=$(git merge-base "${BASELINE:-master}" HEAD)
 set +x
-# \[ "$BUILDKITE_BRANCH" != "master" \] && \[ -n "\$BASELINE" \] && echo "PR build on '$BUILDKITE_BRANCH' branch; base is \$MERGE_BASE (merge-base of '\$BASELINE')" && BASELINE=\$MERGE_BASE
-# \[ "$BUILDKITE_BRANCH" != "master" \] && \[ -z "\$BASELINE" \] && echo "manual build on '$BUILDKITE_BRANCH' branch; using merge-base of master as base (\$MERGE_BASE)" && BASELINE=\$MERGE_BASE
-# \[ "$BUILDKITE_BRANCH" == "master" \] && echo "build on master; using 'master~1' as base branch" && BASELINE=master~1
+if [ "$SEMGREP_BRANCH" == "master" ] ; then
+  echo "build on master; using 'master~1' as base branch" && BASELINE=master~1
+elif [ "$SEMGREP_BRANCH" != "master" ] && [ -n "$BASELINE" ] ; then
+  echo "PR build on '$SEMGREP_BRANCH' branch; base is $MERGE_BASE (merge-base of '$BASELINE')" && BASELINE=$MERGE_BASE
+elif [ "$SEMGREP_BRANCH" != "master" ] && [ -z "$BASELINE" ] ; then
+  echo "manual build on '$SEMGREP_BRANCH' branch; using merge-base of master as base ($MERGE_BASE)" && BASELINE=$MERGE_BASE
+fi
 # python -m semgrep_agent --publish-token "\$SEMGREP_TOKEN" --publish-deployment \$SEMGREP_ID --baseline-ref \$BASELINE
