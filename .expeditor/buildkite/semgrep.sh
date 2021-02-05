7536dc9fe45d50b8c1cfabc463a944f40d924437
Michael Sorens <msorens@chef.io>
2021-02-04 18:05:23 -0800

    buildkite vars--trial 2
    Signed-off-by: michael sorens <msorens@chef.io>

--- .expeditor/buildkite/semgrep.sh
+++ .expeditor/buildkite/semgrep.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -eou pipefail
 
-set -x
+# usage: semgrep.sh $BUILDKITE_BRANCH $BUILDKITE_BUILD_URL $BUILDKITE_PULL_REQUEST_BASE_BRANCH
+
 echo "running in $(pwd)"
-export SEMGREP_BRANCH=$BUILDKITE_BRANCH
+set -x
+export SEMGREP_BRANCH=$1
 export SEMGREP_REPO_NAME=chef/automate
 # Activates links to buildkite builds in slack notification
-export SEMGREP_JOB_URL=$BUILDKITE_BUILD_URL
+export SEMGREP_JOB_URL=$2
 # Activates links to git commits in slack notification
 export SEMGREP_REPO_URL=https://github.com/chef/automate
-export BASELINE=$BUILDKITE_PULL_REQUEST_BASE_BRANCH
-export MERGE_BASE=$(git merge-base ${BASELINE:-master} head)
+BASELINE=$3
+MERGE_BASE=$(git merge-base ${BASELINE:-master} HEAD)
 set +x
 # \[ "$BUILDKITE_BRANCH" != "master" \] && \[ -n "\$BASELINE" \] && echo "PR build on '$BUILDKITE_BRANCH' branch; base is \$MERGE_BASE (merge-base of '\$BASELINE')" && BASELINE=\$MERGE_BASE
 # \[ "$BUILDKITE_BRANCH" != "master" \] && \[ -z "\$BASELINE" \] && echo "manual build on '$BUILDKITE_BRANCH' branch; using merge-base of master as base (\$MERGE_BASE)" && BASELINE=\$MERGE_BASE
