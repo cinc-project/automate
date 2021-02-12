c1735da15cb1b488a2a826be1a687d888d9c1dc6
danielsdeleo <dan@chef.io>
2021-02-12 14:45:26 -0800

    Implement stub gRPC server for new notifications service
    Signed-off-by: Daniel DeLeo <dan@chef.io>

--- components/notifications-service2/cmd/notifications-service/commands/serve.go
+++ components/notifications-service2/cmd/notifications-service/commands/serve.go
@@ -1,11 +1,8 @@
 package commands
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/chef/automate/components/automate-deployment/pkg/toml"
 	"github.com/chef/automate/components/notifications-service2/pkg/config"
+	"github.com/chef/automate/components/notifications-service2/pkg/server"
 	"github.com/spf13/cobra"
 )
 
@@ -35,11 +32,6 @@ func runServeCommand(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	for {
-		fmt.Println("hello from notifications-service2")
-		s, _ := toml.Marshal(c)
-		fmt.Println(string(s))
-		time.Sleep(5 * time.Second)
-	}
-	return nil
+
+	return server.RunGRPCServer(c)
 }
