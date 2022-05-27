09e691cfbb77a6118d1944ded74540451eba6778
bvtejaswi <100406225+bvtejaswi@users.noreply.github.com>
2022-05-27 17:39:02 +0530

    Update components/automate-ui/src/app/entities/infra-nodes/infra-nodes.effects.ts
    Co-authored-by: vivekshankar1 <59958706+vivekshankar1@users.noreply.github.com>

--- components/automate-ui/src/app/entities/infra-nodes/infra-nodes.effects.ts
+++ components/automate-ui/src/app/entities/infra-nodes/infra-nodes.effects.ts
@@ -156,8 +156,7 @@ export class InfraNodeEffects {
   updateNodeEnvironmentSuccess$ = createEffect(() =>
     this.actions$.pipe(
       ofType(NodeActionTypes.UPDATE_ENVIRONMENT_SUCCESS),
-       // eslint-disable-next-line no-empty-pattern    
-      map(({ }: UpdateNodeEnvironmentSuccess) => new CreateNotification({
+      map(() => new CreateNotification({
         type: Type.info,
         message: 'Successfully updated node environment.'
       }))));
