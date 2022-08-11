3970f26dc02aaaaf6824467118c202986cca52f7
VivekSharma2607 <sharmavishu760@gmail.com>
2022-08-11 14:59:55 +0530

    Updates for Asset List API
    Signed-off-by: VivekSharma2607 <sharmavishu760@gmail.com>

--- components/compliance-service/api/reporting/server/server.go
+++ components/compliance-service/api/reporting/server/server.go
@@ -939,7 +939,14 @@ func (srv *Server)AssetCount(ctx context.Context , in *reporting.ListFilters) (*
 func (srv *Server)ListAsset(ctx context.Context , in *reporting.AssetListRequest) (*reporting.AssetList , error) {
 	formattedFilters := formatFilters(in.Filters)
 	var asset []*reporting.Assets
-	formattedFilters, err := filterByProjects(ctx , formattedFilters)
+	endTime := time.Now().Format(time.RFC3339)
+	formattedFilters["end_time"] = []string{endTime}
+	err := relaxting.ValidateTimeRangeForFilters(formattedFilters["start_time"][0] , endTime)
+	if err != nil {
+		logrus.Errorf("The starttime and endtime validation error %v" , err)
+		return nil , err
+	}
+	formattedFilters, err = filterByProjects(ctx , formattedFilters)
 	if err != nil {
 		logrus.Errorf("Unable to get filters by filterbyProject %v" , err)
 		return nil, err
