c4b04e98f53f85cad9db0f3270f14abd4fe34c52
Arvinth C <arvinth.chandrasekaran@progress.com>
2022-09-16 01:00:21 +0530

    Adding comments and terraform fommating
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- components/automate-cli/cmd/chef-automate/initConfigHaTmpl.go
+++ components/automate-cli/cmd/chef-automate/initConfigHaTmpl.go
@@ -370,7 +370,8 @@ access_key = ""
 secret_key = ""
 endpoint = ""
 
-# [Optional] Mention object_storage region Eg: region = "us-west-1"
+# [Optional] Mention object_storage region 
+# Eg: region = "us-west-1"
 region = ""
 
 `
