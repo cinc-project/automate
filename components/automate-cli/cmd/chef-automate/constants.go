f6184968d4986138cb6a9651fd961fe599ad1c88
Arvinth C <arvinth.chandrasekaran@progress.com>
2023-06-05 22:39:12 +0530

    Syncing configuration to the new node after node add
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- components/automate-cli/cmd/chef-automate/constants.go
+++ components/automate-cli/cmd/chef-automate/constants.go
@@ -1,13 +1,17 @@
 package main
 
 const (
-	FRONTEND      = "frontend"
-	AUTOMATE      = "automate"
-	CHEF_SERVER   = "chef_server"
-	POSTGRESQL    = "postgresql"
-	OPENSEARCH    = "opensearch"
-	SET           = "set"
-	PATCH         = "patch"
-	SUDO_PASSWORD = "sudo_password"
-	IPV4REGEX     = `^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
+	FRONTEND         = "frontend"
+	AUTOMATE         = "automate"
+	CHEF_SERVER      = "chef_server"
+	POSTGRESQL       = "postgresql"
+	OPENSEARCH       = "opensearch"
+	AUTOMATE_TOML    = "automate.toml"
+	CHEF_SERVER_TOML = "chef_server.toml"
+	POSTGRESQL_TOML  = "postgresql.toml"
+	OPENSEARCH_TOML  = "opensearch.toml"
+	SET              = "set"
+	PATCH            = "patch"
+	SUDO_PASSWORD    = "sudo_password"
+	IPV4REGEX        = `^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
 )
