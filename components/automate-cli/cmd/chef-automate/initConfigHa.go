3b34caef6bd2a5b1b64003d6691b18dcc9dd1933
punitmundra <pmundra@progress.com>
2023-08-16 22:56:17 +0530

    template change
    Signed-off-by: punitmundra <pmundra@progress.com>

--- components/automate-cli/cmd/chef-automate/initConfigHa.go
+++ components/automate-cli/cmd/chef-automate/initConfigHa.go
@@ -161,8 +161,22 @@ type ExistingInfraConfigInitialsToml struct {
 	BackupMount                 string `toml:"backup_mount,omitempty"`
 	HabitatUIDGid               string `toml:"habitat_uid_gid,omitempty"`
 	BackupConfig                string `toml:"backup_config,omitempty"`
+	ServiceAccount              string `toml:"service_account",omitempty`
 }
 
+type ServiceAccount struct {
+	Type                    string `json:"type"`
+	ProjectID               string `json:"project_id"`
+	PrivateKeyID            string `json:"private_key_id"`
+	PrivateKey              string `json:"private_key"`
+	ClientEmail             string `json:"client_email"`
+	ClientID                string `json:"client_id"`
+	AuthURI                 string `json:"auth_uri"`
+	TokenURI                string `json:"token_uri"`
+	AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
+	ClientX509CertURL       string `json:"client_x509_cert_url"`
+	UniverseDomain          string `json:"universe_domain"`
+}
 type ExistingInfraAutomateToml struct {
 	Config ExistingInfraAutomateConfigToml `toml:"config"`
 }
