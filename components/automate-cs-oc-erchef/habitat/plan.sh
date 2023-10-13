1f7336f32bf662907549b58cfecbbbdcc28888e0
RajeshPaul38 <rajesh.paul@progress.com>
2023-10-13 11:19:49 +0530

    erchef from rpaul

--- components/automate-cs-oc-erchef/habitat/plan.sh
+++ components/automate-cs-oc-erchef/habitat/plan.sh
@@ -18,7 +18,8 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_erchef/15.8.0/20230929110950"
+  # "${vendor_origin}/oc_erchef/15.8.0/20230929110950"
+  "rpaul/oc_erchef/15.8.0/20231010105643"
 )
 
 pkg_build_deps=(
